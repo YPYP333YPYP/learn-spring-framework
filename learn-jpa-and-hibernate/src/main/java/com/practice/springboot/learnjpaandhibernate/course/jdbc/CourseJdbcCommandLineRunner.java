230506193c2a3c5882f072191e00ba8d533830d0
YPYP333YPYP <ypyp333ypyp@gmail.com>
2023-09-16 17:02:44 +0900

    Spring Boot Jdbc 삽입 -2

--- learn-jpa-and-hibernate/src/main/java/com/practice/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/practice/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.practice.springboot.learnjpaandhibernate.course.Course;
+
 @Component
 public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 	
@@ -12,7 +14,11 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
-		repository.insert();
+		repository.insert(new Course(1, "Learn AWS", "practice"));
+		repository.insert(new Course(2, "Learn GDC", "practice"));
+		repository.insert(new Course(3, "Learn Azure", "practice"));
+		
+		repository.delete(1);
 		
 	}
 	
