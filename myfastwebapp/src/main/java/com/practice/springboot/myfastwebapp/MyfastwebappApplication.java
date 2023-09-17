2aabedd6ba6cf36a872d7ed3ccfdcbcdeddd89c4
YPYP333YPYP <ypyp333ypyp@gmail.com>
2023-09-17 23:54:17 +0900

    Spring Boot Web 설정

---
+++ myfastwebapp/src/main/java/com/practice/springboot/myfastwebapp/MyfastwebappApplication.java
@@ -0,0 +1,13 @@
+package com.practice.springboot.myfastwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfastwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfastwebappApplication.class, args);
+	}
+
+}
---
+++ myfastwebapp/src/test/java/com/practice/springboot/myfastwebapp/MyfastwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.springboot.myfastwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfastwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
