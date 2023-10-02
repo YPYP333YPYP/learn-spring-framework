ebdd15eb5783fb18678caa0fb5717d55ed6f1fd4
YPYP333YPYP <ypyp333ypyp@gmail.com>
2023-10-02 13:35:54 +0900

    Spring Security CSRF 사용하기

--- learn-spring-security/src/main/java/com/practice/learnspringsecurity/resources/HelloWorldResource.java
+++ learn-spring-security/src/main/java/com/practice/learnspringsecurity/resources/HelloWorldResource.java
@@ -1,4 +1,4 @@
-package com.practice.learnspringsecurity;
+package com.practice.learnspringsecurity.resources;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
