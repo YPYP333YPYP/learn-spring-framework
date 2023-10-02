efdb411df826886c2d363d6f1a993e84eeeabea0
YPYP333YPYP <ypyp333ypyp@gmail.com>
2023-10-02 13:50:01 +0900

    Spring Security CSRF 사용 해제

---
+++ learn-spring-security/src/main/java/com/practice/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.practice.learnspringsecurity.basic;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class BasicAuthSecurityConfiguration {
+	
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		
+		http.authorizeHttpRequests(
+						auth -> {
+							auth.anyRequest().authenticated();
+						});
+		
+		http.sessionManagement(
+						session -> 
+							session.sessionCreationPolicy(
+									SessionCreationPolicy.STATELESS)
+						);
+		
+		//http.formLogin();
+		http.httpBasic(withDefaults());
+		
+		http.csrf(csrf -> csrf.disable());
+		
+		return http.build();
+	}
+
+
+}
