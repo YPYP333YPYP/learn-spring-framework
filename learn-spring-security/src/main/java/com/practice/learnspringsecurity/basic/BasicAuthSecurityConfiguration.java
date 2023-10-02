b665a35c37d26b69f826c2ca9edb31dd9abc7b23
YPYP333YPYP <ypyp333ypyp@gmail.com>
2023-10-02 15:03:54 +0900

    Spring Security 사용자 정보 - 메모리

--- learn-spring-security/src/main/java/com/practice/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ learn-spring-security/src/main/java/com/practice/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -31,6 +34,25 @@ public class BasicAuthSecurityConfiguration {
 		http.csrf(csrf -> csrf.disable());
 		
 		return http.build();
+	}
+	
+	// UserDetailsService 클래스를 이용해 User에 대한 객체 생성이 가능하다.
+	// InMemoryUserDetailsManager는 사용자 정보를 메모리에 저장할 수 있다. 
+	@Bean
+	public UserDetailsService userDetailService() {
+		
+		var user = User.withUsername("Lee")
+			.password("{noop}dummy")
+			.roles("USER")
+			.build();
+
+		
+		var admin = User.withUsername("admin")
+				.password("{noop}dummy")
+				.roles("ADMIN")
+				.build();
+
+		return new InMemoryUserDetailsManager(user, admin);
 	}
 
 
