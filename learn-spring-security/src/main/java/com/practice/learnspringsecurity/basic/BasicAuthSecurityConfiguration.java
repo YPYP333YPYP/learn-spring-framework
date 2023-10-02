b362910eb1870bafab86199714b7f1ed8d60b733
YPYP333YPYP <ypyp333ypyp@gmail.com>
2023-10-02 15:21:30 +0900

    Spring Security 사용자 정보 - DB저장

--- learn-spring-security/src/main/java/com/practice/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ learn-spring-security/src/main/java/com/practice/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -1,13 +1,20 @@
 package com.practice.learnspringsecurity.basic;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -33,26 +40,42 @@ public class BasicAuthSecurityConfiguration {
 		
 		http.csrf(csrf -> csrf.disable());
 		
+		// H2 콘솔 페이지를 이용하기 위해서 frame 옵션을 해제한다.
+		http.headers(headers -> headers.frameOptions(frameOptionsConfig-> frameOptionsConfig.disable()));
+		
 		return http.build();
 	}
 	
-	// UserDetailsService 클래스를 이용해 User에 대한 객체 생성이 가능하다.
-	// InMemoryUserDetailsManager는 사용자 정보를 메모리에 저장할 수 있다. 
+	// DataSource 객체를 이용하여 데이터베이스를 H2로 설정하고 JDBC의 UserDetails와 관련된 스크립트 코드를 빌드하여 
+	// 사용자 정보가 담긴 "users"테이블과 자격증명 내용이 담긴 "authorities" 테이블을 생성할 수 있다. 
 	@Bean
-	public UserDetailsService userDetailService() {
+	public DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder()
+				.setType(EmbeddedDatabaseType.H2)
+				.addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+				.build();
+	}
+	
+	// 이번엔 JDBC를 이용하여 사용자 정보를 저장할 것이기 때문에 JdbcUserDetailsManager 객체를 만들고 
+	// dataSource를 매개변수로 받아와 생성한 User 객체를 데이터베이스에 저장할 수 있다. 
+	@Bean
+	public UserDetailsService userDetailService(DataSource dataSource) {
 		
 		var user = User.withUsername("Lee")
 			.password("{noop}dummy")
 			.roles("USER")
 			.build();
-
 		
 		var admin = User.withUsername("admin")
 				.password("{noop}dummy")
-				.roles("ADMIN")
+				.roles("ADMIN", "USER")
 				.build();
+		
+		var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+		jdbcUserDetailsManager.createUser(user);
+		jdbcUserDetailsManager.createUser(admin);
 
-		return new InMemoryUserDetailsManager(user, admin);
+		return jdbcUserDetailsManager;
 	}
 
 
